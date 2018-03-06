be265564b47c93cebae542e3e8604ea0e32ad8f2
Julio Cachon Villadangos <juliocachonvi@gmail.com>
2018-03-06 22:31:54 +0100

    Commit 01, 0543.b, agregado metodo que nos muestra la url mas visitada

--- AnalizadorAccesosAServidor.java
+++ AnalizadorAccesosAServidor.java
@@ -82,7 +82,30 @@ public class AnalizadorAccesosAServidor
      */
     public String paginaWebMasSolicitada() 
     {
-        return "";
+        String paginaWebMasAccesos = null;
+        HashMap<String, Integer> urlsYAccesos = new HashMap<>();
+        int numeroAccesosUrl = 0;
+        if(accesos.size() > 0){
+            for(Acceso urlActual : accesos){
+                String urlAcceso = urlActual.getUrl();
+
+                if(urlsYAccesos.get(urlAcceso) == null){
+                    urlsYAccesos.put(urlAcceso, 1);
+                }
+                else{
+                    urlsYAccesos.replace(urlAcceso, urlsYAccesos.get(urlAcceso) + 1);                    
+                }
+
+                if(urlsYAccesos.get(urlAcceso) > numeroAccesosUrl){
+                    paginaWebMasAccesos = urlAcceso;
+                    numeroAccesosUrl = urlsYAccesos.get(urlAcceso);
+                }
+            }
+        }
+        else{
+            System.out.println("Ocurrio algun error al leer el archivo, no hay datos que procesar.");
+        }
+        return paginaWebMasAccesos;
     }
 
     /**
